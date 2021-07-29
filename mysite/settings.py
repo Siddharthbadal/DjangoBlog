01b609b9df67ed42eeb418600e067e259f492914
Sidd <siddharthabadal.2015@gmail.com>
2021-07-29 13:58:41 +0530

    commits

--- mysite/settings.py
+++ mysite/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 
 django_heroku.settings(locals())
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
@@ -134,14 +134,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
-
 # Location of static files
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
-
 
 STATIC_ROOT  = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
